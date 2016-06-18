ee1049b6af1f3994f8137462f919d3e5158dcaef
Jacob Carey <jacobcvt12@gmail.com>
2016-06-17 21:58:19 -0400

    baseline sim/model

--- reliability.R
+++ reliability.R
@@ -3,42 +3,30 @@ library(R2jags)
 
 # https://faculty.franklin.uga.edu/dhall/sites/faculty.franklin.uga.edu.dhall/files/ZIMixed.pdf
 set.seed(1)
-#n <- 1000
-n <- 299
+n <- 200
 pts <- rpois(n, 100)
 
 # overall beta parameters
-a <- 5
-b <- 70
+a <- 25
+b <- 5
 
 # shared probability of structural zero
 phi <- 0.1
 
 theta <- rbeta(n, a, b)
-pass <- rbinom(n, pts, theta)
 #pass <- rzibinom(n, pts, theta, pstr0=phi)
+pass <- rbinom(n, pts, theta)
 
 model <- function() {
-    mean.rel <- mean(rel)
-
-    for (i in 1:n) {
-        var.binom[i] <- theta[i] * (1 - theta[i]) / pts[i]
-        rel[i] <- p.to.p.var / (p.to.p.var + var.binom[i])
-    }
-
-    p.to.p.var <- a * b / ((a + b + 1) * (a + b) ^ 2)
-
     for (i in 1:n) {
         pass[i] ~ dbinom(theta[i], pts[i])
-    }
-
-    for (i in 1:n) {
         theta[i] ~ dbeta(a, b)
     }
 
     # priors
     a ~ dunif(0, 1000)
     b ~ dunif(0, 1000)
+    #phi[1:2] ~ ddirch(
 }
 
 model.data <- c("pass", "pts", "n")
@@ -46,9 +34,11 @@ model.inits <- function() {
     inits <- list()
     inits$a <- runif(1, 0, 1000)
     inits$b <- runif(1, 0, 1000)
+#    inits$phi <- rbeta(1, 1)
     return(inits)
 }
-model.params <- c("a", "b", "p.to.p.var", "mean.rel")
+model.params <- c("a", "b")
+#model.params <- c("a", "b", "phi")
 
 fit <- jags(model.data, model.inits, model.params, model, n.iter=5000)
 fit
