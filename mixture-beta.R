5b15faf7cfc695a902d015ad0bcb252a365a4064
Jacob Carey <jacobcvt12@gmail.com>
2016-06-23 22:07:06 -0400

    Make mixture harder

--- mixture-beta.R
+++ mixture-beta.R
@@ -1,23 +1,24 @@
 library(R2jags)
 library(ggplot2)
+library(shinystan)
 
 set.seed(1)
 n <- 100
 
 # latent classes
-k <- 2
-p <- c(1/3, 1-1/3)
+k <- 3
+p <- c(1/4, 1/2, 1/4)
 z <- sample(seq_len(k), n, replace=TRUE, prob=p)
 alpha <- rep(1, k)
 
 # overall beta parameters
-a <- c(2, 40)
-b <- c(50, 10)
+a <- c(1, 50, 99)
+b <- c(99, 50, 1)
 
 # generate theta
 theta <- rbeta(n, a[z], b[z])
-ggplot(data.frame(theta), aes(theta)) +
-    geom_density()
+#ggplot(data.frame(theta), aes(theta)) +
+#       geom_density()
 
 model <- function() {
     for (i in 1:n) {
@@ -35,7 +36,8 @@ model <- function() {
 }
 
 model.data <- c("theta", "alpha", "k", "n")
-model.params <- c("a", "b", "pi")
+model.params <- c("a", "b", "pi", "z")
 
-fit <- jags(model.data, NULL, model.params, model, n.iter=20000)
-fit
+fit <- jags(model.data, NULL, model.params, model, n.iter=500000)
+saveRDS(fit, file="mixture-beta.RDS")
+#launch_shinystan(as.shinystan(as.mcmc(fit)))
