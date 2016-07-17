cf0a23b720cdd49be5e44e940340506f6c14e9d4
Jacob Carey <jacobcvt12@gmail.com>
2016-07-17 13:54:09 -0400

    Update infinite mixture model

--- R/mixture-hbeta.R
+++ R/mixture-hbeta.R
@@ -1,30 +1,11 @@
 library(R2jags)
 library(shinystan)
 
-set.seed(42)
-n <- 50
+# read in data
+X <- readRDS("data/binomial.RDS")
+n <- length(X)
 
-# latent classes
-k <- 3
-p <- rep(1/k, k)
-z <- sample(seq_len(k), n, replace=TRUE, prob=p)
-
-# overall beta parameters
-a <- c(1, 5, 9)
-b <- c(9, 5, 1)
-
-# calculate true overall means and variance of the mixture
-means <- a / (a + b)
-variances <- means * (1 - means) / (a + b + 1)
-mean.overall <- means %*% p
-var.overall <- p %*% (means^2 + variances) - mean.overall^2
-
-# generate theta
-theta <- rbeta(n, a[z], b[z])
-
-# generate binomials
-X <- rbinom(n, 100, theta)
-
+# "infinite" mixture of beta binomial model
 model <- function() {
     for (i in 1:n) {
         X[i] ~ dbinom(theta[i], 100)
@@ -67,11 +48,18 @@ model <- function() {
     pi ~ ddirch(alpha[])
 }
 
-model.data <- c("X", "alpha", "k", "n")
-model.params <- c("mu.overall", "var.overall", "a", "b", "pi", "mu", "sigma.2")
-model.params <- c("var.overall")
-k <- 20
-alpha <- rep(1, k)
+k <- 20 # number of components approx infinity
+alpha <- rep(1, k) # prior on pi
 
+model.data <- c("X", "alpha", "k", "n")
+model.params <- c("var.overall", "theta")
+
+# fit model
+set.seed(42)
 fit <- jags(model.data, NULL, model.params, model, n.iter=2000)
+
+# diagnose model
 launch_shinystan(as.shinystan(as.mcmc(fit)))
+
+# save results
+saveRDS(fit, "output/inf-mix-beta-binomial.RDS")
